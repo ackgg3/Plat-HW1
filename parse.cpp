05f077dbbd4bd93877582b6f908383779a7f0462
ackgg3 <ackgg3@mst.edu>
2018-02-12 18:04:05 -0600

    Restructured how the determining is done, it is now functionalized

--- parse.cpp
+++ parse.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 
-const string INTEGER = "Integer";
-const string DECIMAL = "Decimal";
+const string ING = "Integer";
+const string DEC = "Decimal";
 const string HEX     = "Hexidecimal";
-const string SCIENCE = "Scientific";
-const string PHONE   = "Phone";
-const string IDENT   = "Identifier";
-const string ERR	 = "Unknown";
+const string SCI = "Scientific";
+const string PHN   = "Phone";
+const string IDT   = "Identifier";
+const string KEY    = "Keyword"
+const string ERR	 = "INVALID!";
 int main()
 {	
 	vector<string> input;
@@ -22,10 +23,12 @@ int main()
 	string result = "";
 	while(cin >> word)
 	{
-		input.push_back(word); 		//We get the input and hold it in the vector
-	} //Weep
+		input.push_back(word); 		//We get the input and hold it in the 
+									//vector
+	} 
 	
-	cout << input.size() << endl;  	//Print size because the first thing is the size
+	cout << input.size() << endl;  	//Print size because the first thing is 
+									//the size
     
 	
 
@@ -33,14 +36,14 @@ int main()
 	while(input.size() > 0)
 	{
 		word = input.front(); //Get the next word
-		for(int i = 0; i < word.length(); i++) //Get word letter by letter
-		{
-			switch(state) //digest word
-			{
-				//Starting over
-				
-			}
-		}
+		if 		(isInt(word)) {result = ING}
+		else if (isDec(word)) {result = DEC}
+		else if (isSci(word)) {result = HEX}
+		else if (isHex(word)) {result = SCI}
+		else if (isPhn(word)) {result = PHN}
+		else if (isKey(word)) {result = IDT}
+		else if (isIdt(word)) {result = KEY}
+		else 				  {result = ERR}
 		output.push_back(result); //record result
 		input.erase(input.begin()); //we don't need that word any more
 	}
@@ -48,7 +51,8 @@ int main()
 	while(output.size() > 0)
 	{	
 		count++;
-		cout << count << ": " << output.front() << endl; //Knowing what's going in is nice
+		cout << count << ": " << output.front() <<"."<< endl;//Knowing what's 
+															//going in is nice
 		output.erase(output.begin()); //BACK TO THE PIT WITH YE
 	}
 	return 0;
